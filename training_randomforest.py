d29ca002acf22032e2b721383a475a8643383ad3
Mohamed <32842839+hedjazi@users.noreply.github.com>
2017-10-16 18:31:36 +0300

    Showing all the scores instead of the accuracy
    We want to see if the a class with a small amount of points is not affected in term of precision, recall and accuracy (accuracy alone is not enough to judge).

--- training_randomforest.py
+++ training_randomforest.py
@@ -2,14 +2,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
 X = np.load('X.npy')
 Y = np.load('Y.npy')
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10, random_state=1000)
-
-accuracy = 0
 
 iteration = 0
 folds = 10
@@ -21,7 +19,6 @@ for train_index, val_index in kf.split(X_train):
     clf = RandomForestClassifier(n_estimators=30, max_depth=None, min_samples_split=2, random_state=0)
     clf.fit(Xtrain, Ytrain)
     Y_estimated = clf.predict(Xval)
-    accuracy += accuracy_score(Yval, Y_estimated)
+    target_names = ['class 0', 'class 1', 'class 2']
+    print classification_report(Yval, Y_estimated, target_names=target_names)
     iteration += 1
-
-print 'Accuracy: ', (accuracy/folds)
