ec28c4170757aad7adb15fbc3f66f515cd8ddb06
Mohamed <32842839+hedjazi@users.noreply.github.com>
2017-10-17 16:51:44 +0300

    Generating more data of class 1

--- data_generator.py
+++ data_generator.py
@@ -27,13 +27,31 @@ def count_minus(coefs):
             count+=1
     return count
 
+def generate_more_doublesol():
+    # we have delta = b**2 - 4*a*c, this means if we want to generate double solution coefs, delta = 0
+    # ====> b = 2 * sqrt(a*c), ===> we just need to generate random a's (or c's) to get b's, thus b = 2a or b = 2c given that a = c
+    mat = []
+    for a in range(10000):
+        b = 2 * a
+        c = a
+        vec1 = [a, b, c]
+        vec2 = [-1*a, b, -1*c]
+        #d1 = b ** 2 - 4 * a * c
+        #d2 = b ** 2 - 4 * (-1*a) * (-1*c)
+        mat.append(vec1)
+        mat.append(vec2)
+    return mat
+
 if __name__ == "__main__":
     size = 100000
     length = 4
     mat = generate_data_as_array(size, length)
     matrix = array_to_matrix( mat )
     matrix = np.array( matrix, dtype=int )
-    
+
+    mat_double_sol = np.array( generate_more_doublesol() )
+
+    matrix = np.concatenate((matrix, mat_double_sol), axis=0)
     X = []
     Y = []
     for coefs in matrix:
@@ -57,5 +75,5 @@ if __name__ == "__main__":
         arr.append( 3 - count_minus( coefs ) ) # positives
         X.append(arr)
         Y.append(y)
-    np.save('X_additional_position.npy', X)
-    np.save('Y_additional_position.npy', Y)
+    np.save('X_additional_position_doublesol.npy', X)
+    np.save('Y_additional_position_doublesol.npy', Y)
